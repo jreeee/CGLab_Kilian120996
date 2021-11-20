ed57e63ab6aa2b8328b180f8e0f3470e8699fe45
jreeee <jeremias.michael.kilian@uni-weimar.de>
2021-11-20 21:25:40 +0100

    started working on orbits

--- application/source/application_solar.cpp
+++ application/source/application_solar.cpp
@@ -61,23 +61,6 @@ void ApplicationSolar::render() const {
   renderStars();
   renderPlanet();
 
-  // glUseProgram(m_shaders.at("planet").handle);
-
-  //   glm::fmat4 model_matrix = glm::rotate(glm::fmat4{}, float(glfwGetTime()), glm::fvec3{-1.0f, 0.0f, 0.0f});
-  //   model_matrix = glm::translate(model_matrix, glm::fvec3{0.0f, 0.0f, -1.0f});
-  //   glUniformMatrix4fv(m_shaders.at("planet").u_locs.at("ModelMatrix"),
-  //                     1, GL_FALSE, glm::value_ptr(model_matrix));
-
-  //   // extra matrix for normal transformation to keep them orthogonal to surface
-  //   glm::fmat4 normal_matrix = glm::inverseTranspose(glm::inverse(m_view_transform) * model_matrix);
-  //   glUniformMatrix4fv(m_shaders.at("planet").u_locs.at("NormalMatrix"),
-  //                     1, GL_FALSE, glm::value_ptr(normal_matrix));
-
-  //   // bind the VAO to draw
-  //   glBindVertexArray(planet_object.vertex_AO);
-
-  //   // draw bound vertex array using bound shader
-  //   glDrawElements(planet_object.draw_mode, planet_object.num_elements, model::INDEX.type, NULL);
 }
 
 
@@ -273,6 +256,14 @@ void ApplicationSolar::initializeShaderPrograms() {
   // request uniform locations for shader program
   m_shaders.at("star").u_locs["ModelViewMatrix"] = -1;
   m_shaders.at("star").u_locs["ProjectionMatrix"] = -1;
+
+  m_shaders.emplace("orbit", shader_program{{{GL_VERTEX_SHADER,m_resource_path + "shaders/orbit.vert"},
+                                           {GL_FRAGMENT_SHADER, m_resource_path + "shaders/vao.frag"}}});
+                                           // request uniform locations for shader program
+  m_shaders.at("planet").u_locs["NormalMatrix"] = -1;
+  m_shaders.at("planet").u_locs["ModelMatrix"] = -1;
+  m_shaders.at("planet").u_locs["ViewMatrix"] = -1;
+  m_shaders.at("planet").u_locs["ProjectionMatrix"] = -1;
 }
 
 // load models
---
+++ resources/shaders/orbit.vert
@@ -0,0 +1,16 @@
+#version 150
+#extension GL_ARB_explicit_attrib_location : require
+// vertex attributes of VAO
+layout(location = 0) in vec3 in_Position;
+layout(location = 1) in vec3 in_Color;
+
+//Matrix Uniforms uploaded with glUniform*
+uniform mat4 ModelViewMatrix;
+uniform mat4 ProjectionMatrix;
+out vec3 pass_Color;
+
+void main(void)
+{
+	gl_Position = ProjectionMatrix * ModelViewMatrix * vec4(in_Position, 1.0);
+	pass_Color = in_Color;
+}
